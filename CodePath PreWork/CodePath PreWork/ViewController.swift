81b96f580fc3fd41a91ebf4d4e422836dbf6ce11
Temitayo-bit <146789049+Temitayo-bit@users.noreply.github.com>
2025-01-06 00:00:15 -0500

    Add files via upload

--- CodePath PreWork/CodePath PreWork/ViewController.swift
+++ CodePath PreWork/CodePath PreWork/ViewController.swift
@@ -9,28 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    
-    @IBOutlet weak var changeBackgroundColor: UIButton! 
-    
-    func changeColor() -> UIColor{
+    // IBOutlet for the button
+    @IBOutlet weak var changeBackgroundColor: UIButton!
 
-            let red = CGFloat.random(in: 0...1)
-            let green = CGFloat.random(in: 0...1)
-            let blue = CGFloat.random(in: 0...1)
+    // Function to generate a random color
+    func changeColor() -> UIColor {
+        let red = CGFloat.random(in: 0...1)
+        let green = CGFloat.random(in: 0...1)
+        let blue = CGFloat.random(in: 0...1)
 
-            return UIColor(red: red, green: green, blue: blue, alpha: 0.5)
-        }
+        return UIColor(red: red, green: green, blue: blue, alpha: 0.5)
+    }
 
-
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        let randomColor = changeColor()
-            view.backgroundColor = randomColor
-        // Do any additional setup after loading the view.
+        // Set an initial background color when the view loads
+        view.backgroundColor = changeColor()
     }
-    
 
-
+    // IBAction for the button tap
+    @objc @IBAction func didTapChangeBackground(_ sender: UIButton) {
+        // Change the background color each time the button is tapped
+        view.backgroundColor = changeColor()
+    }
 }
-
